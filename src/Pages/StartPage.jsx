86769a1a24fa298c4e153a801cb4d2b83c5b24ec
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2024-03-17 02:01:00 +0600

    added home page

--- src/Pages/StartPage.jsx
+++ src/Pages/StartPage.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/goodKnight.png";
+import Logo from "../components/Logo"
+// import logo from "../assets/goodKnight.png";
 const StartPage = () => {
     return (
         <div className="h-dvh bg-[#890000]">
-            <div className="flex items-center justify-center pt-60 pr-5">
-                <img className="w-48" src={logo} alt="goodKnight-logo" />
+            <div className="flex items-center justify-center pt-52 pr-5">
+                <Logo />
             </div>
             <div className="text-center mt-10">
                 <h2 className="text-white text-2xl font-normal">POWER ACTIV +</h2>
@@ -12,7 +13,7 @@ const StartPage = () => {
                     Door to door sales activation
                 </h4>
             </div>
-            <div className="text-center mt-32">
+            <div className="text-center mt-20">
                 <Link to='/signin'>
                     <button className="bg-gradient-to-r from-[#FF5454] to-[#E10000] text-white text-xl font-bold px-20 py-2 rounded-xl shadow-xl">
                         ENTER
