13a19146692ddfbc28b028bfd58bd45dc5f838bc
Himel <khondokarh55@gmail.com>
2024-03-16 21:59:13 +0600

    sign in page added

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import SignIn from '../components/SignIn';
+
+const Homepage = () => {
+    return (
+        <div className='bg-[#890000] flex flex-col items-center pt-[50px] h-screen'>
+            <div className='pr-8 relative'>
+                <Logo></Logo>
+            </div>
+            <div className=' space-y-4 text-center'>
+                <h1 className='text-white mt-4 text-2xl'>POWER ACTIVE +</h1>
+                <h3 className='text-white text-base'>Door to door sales activation</h3>
+            </div>
+
+            <div>
+                <SignIn></SignIn>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Homepage;
