69d70ee7d4765c3b6322686005de3629fdc6f5e1
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2024-03-16 19:36:15 +0600

    added capcitorjs to convert to an app

--- src/Pages/StartPage.jsx
+++ src/Pages/StartPage.jsx
@@ -9,8 +9,15 @@ const StartPage = () => {
                     alt="goodKnight-logo"
                 />
             </div>
-            <div>
+            <div className='text-center mt-10'>
                 <h2 className='text-white text-2xl font-normal'>POWER ACTIV +</h2>
+                <h4 className='text-white font-normal mt-5'>Door to door sales activation</h4>
+            </div>
+            <div className='text-center mt-32'>
+                <button
+                    className='bg-gradient-to-r from-[#FF5454] to-[#E10000] text-white text-xl font-bold px-20 py-2 rounded-xl shadow-xl'>
+                    ENTER
+                </button>
             </div>
         </div>
     );
