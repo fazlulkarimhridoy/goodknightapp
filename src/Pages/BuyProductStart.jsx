a52bcd757b71e16f6ddc6d85f0c8088ed5c41062
Himel <khondokarh55@gmail.com>
2024-03-19 18:02:42 +0600

    updated

--- src/Pages/BuyProductStart.jsx
+++ src/Pages/BuyProductStart.jsx
@@ -13,11 +13,26 @@ const BuyProductStart = () => {
   }
 
   return (
-    <div className="container">
-      <div className="pr-14 relative">
-        <Logo width={"w-[136px]"} height={"h-[200px]"}></Logo>
-      </div>
-    </>
+<>
+<Navbar></Navbar>
+<div className="container">
+  <div className="pr-12 relative">
+    <Logo width={"w-[136px]"} height={"h-[200px]"}></Logo>
+  </div>
+  <div className="text-center">
+    <p className="text-white mt-4 text-2xl p-2">
+      Are you interested to buy
+    </p>
+    <p className="text-white mt-4 text-4xl p-2">GoodKnight POWER ACTIV+ ?</p>
+  </div>
+  <div className="flex justify-between gap-10">
+    <button className="text-xl text-white bg-[#303030] px-8 py-2 flex justify-center items-center gap-2"> <ImCross /><span className="text-2xl">NO</span></button>
+    <Link to="/consumerform">
+      <button className="text-3xl text-white bg-[#2C9A1A] px-8 py-2 flex justify-center items-center gap-2"> <TiTick /><span className="text-2xl">YES</span></button>
+    </Link>
+  </div>
+</div>
+</>
 
   );
 };
