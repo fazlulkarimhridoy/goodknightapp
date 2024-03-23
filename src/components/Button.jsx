ad8e92aa50d6723b1ea1a37414bc3ea716621dae
Himel <khondokarh55@gmail.com>
2024-03-24 01:51:35 +0600

    ocr added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Button = ({title}) => {
+const Button = ({title ,func}) => {
   return (
     
-      <button className="w-[300px] text-white text-xl font-bold border-none bg-gradient-to-r from-[#FF5454] to-[#E10000] py-3 rounded-xl outline-none">
+      <button onClick={func} className="btn w-[300px] text-white text-xl font-bold border-none bg-gradient-to-r from-[#FF5454] to-[#E10000] py-3 rounded-xl outline-none">
         {title}
       </button>
     
