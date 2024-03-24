1a844b30c51d5dba5b37d5c37367386a32ac6ede
Himel <khondokarh55@gmail.com>
2024-03-25 03:22:23 +0600

    page-swapper-added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { motion } from "framer-motion"
 
-const Button = ({title ,func}) => {
+const Button = ({ title, func }) => {
   return (
-    
-      <button onClick={func} className="btn w-[300px] text-white text-xl font-bold border-none bg-gradient-to-r from-[#FF5454] to-[#E10000] py-3 rounded-xl outline-none">
-        {title}
-      </button>
-    
+    <motion.button
+      whileTap={{ scale: 0.9 }}
+      onClick={func}
+      className="btn w-[300px] text-white text-xl font-bold border-none bg-gradient-to-r from-[#FF5454] to-[#E10000] py-3 rounded-xl outline-none"
+    >
+      {title}
+    </motion.button>
   );
 };
 
